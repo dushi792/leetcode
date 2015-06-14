4074238c5a2dbd3449e1d7394bb78069ca3f2454
Jun Zhao <dushi792@gmail.com>
2015-06-14 16:44:52 -0400

    2 way for 48

--- 48 Rotate Image/Solution.cpp
+++ 48 Rotate Image/Solution.cpp
@@ -14,3 +14,25 @@ public:
         }
     }
 };
+
+//Another way
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < matrix.size(); i++){
+            for (int j = i; j < matrix.size(); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+};
