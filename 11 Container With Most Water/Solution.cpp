282b32ca80f7a902678c081163afce17b1c4172d
Jun Zhao <dushi792@gmail.com>
2015-06-14 16:58:35 -0400

    concise logic

--- 11 Container With Most Water/Solution.cpp
+++ 11 Container With Most Water/Solution.cpp
@@ -3,17 +3,12 @@ public:
     int maxArea(vector<int>& height) {
         int i = 0;
         int j = height.size()-1;
-        int max = 0;
+        int res = 0;
         
         while (i < j) {
-            int curr = min(height[i],height[j]) * (j - i);
-            if (max < curr) {
-                max = curr;
-            }
-            else {
-                height[i] > height[j] ? j--:i++;
-            }
+            res = max(min(height[i],height[j]) * (j - i), res);
+            height[i] > height[j] ? j--:i++;
         }
-        return max;
+        return res;
     }
 };
