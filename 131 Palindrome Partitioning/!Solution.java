f06800036d9d0d015d9c3bd596ef9f1c83aa13d7
Jun Zhao <dushi792@gmail.com>
2015-08-09 20:12:28 -0400

    modify

--- 131 Palindrome Partitioning/!Solution.java
+++ 131 Palindrome Partitioning/!Solution.java
@@ -7,32 +7,29 @@ public class Solution {
         helper(s, 0);
         return res;
     }
-
     public void helper(String s, int index) {
-        if (cur.size()>0 &&index >= s.length()) {
+        //Discuss answer: if(currLst.size()>0 //the initial str could be palindrome
+        if (index >= s.length()) {
             List<String> temp = (ArrayList<String>) cur.clone();
             res.add(temp);
         }
-
         for(int i = index; i < s.length(); i++) {
             if (isPalind(s, index, i)) {
                 if(index==i)
                     cur.add(Character.toString(s.charAt(i)));
                 else
+                // 第i+1个元素不会被复制，只复制到第i个
                     cur.add(s.substring(index,i+1));
-                // cur.add(s.substring(index,i));
-                helper(s, index+1);
+                helper(s, i+1);
                 cur.remove(cur.size()-1);
             }
         }
     }
-
     private boolean isPalind(String s, int begin, int end) {
         if (begin == end) return true;
         while (begin < end) {
             if (s.charAt(begin) != s.charAt(end)) return false;
-            begin++;
-            end--;
+            begin++; end--;
         }
         return true;
     }
