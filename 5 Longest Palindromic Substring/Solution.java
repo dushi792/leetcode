c33d46f52a127541dd4f0103736994d8d8764c59
Jun Zhao <dushi792@gmail.com>
2015-08-24 13:40:46 -0400

    5

--- 5 Longest Palindromic Substring/Solution.java
+++ 5 Longest Palindromic Substring/Solution.java
@@ -1,30 +1,23 @@
-// 超时
 public class Solution {
+    private int index = 0;
+    private int len = 1;
     public String longestPalindrome(String s) {
         if (s == null || s.length() == 0) return s;
-        String str = null;
-        int len = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            int left, right;
-            if (i+1 < s.length() && s.charAt(i) == s.charAt(i+1)) {
-                left = i;
-                right = i+1;
-            }
-            else {
-                left = right = i;
-            }
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) != s.charAt(right)) {
-                    break;
-                }
-                left--;
-                right++;
-            }
+            helper(s, i, i);
+            helper(s, i, i+1);
+        }
+        return s.substring(index, index+len);
+    }
+    private void helper(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             if (right - left + 1 > len) {
                 len = right - left + 1;
-                str = s.substring(left+1, right);
+                index = left;
             }
+            left--;
+            right++;
         }
-        return str;
     }
 }
