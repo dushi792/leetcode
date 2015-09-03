3e5b80be240fc59056d802f2b6999f2e41dde9f9
Jun Zhao <dushi792@gmail.com>
2015-09-03 14:35:05 -0400

    98

---
+++ 98 Validate Binary Search Tree/Solution(复习).java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        TreeNode pre = null;
+        
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            else {
+                cur = stack.pop();
+                if (pre != null && pre.val >= cur.val) {
+                    return false;
+                }
+                pre = cur;
+                cur = cur.right;
+            }
+        }
+        return true;
+    }
+}
