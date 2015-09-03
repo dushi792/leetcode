184ab82a0f29227e238da21283f05bd7787c2c53
Jun Zhao <dushi792@gmail.com>
2015-09-03 14:36:01 -0400

    103

--- 103 Binary Tree Zigzag Level Order Traversal/Solution.java
+++ 103 Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -14,3 +14,49 @@ public class Solution {
         helper(root.right, res, level+1);
     }
 }
+
+// 用Stack实现的iteration
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List result = new ArrayList();
+
+        if (root == null) {
+            return result;
+        }
+        int levelCount = 1;
+        Stack<TreeNode> currLevel = new Stack<TreeNode>();
+        Stack<TreeNode> nextLevel = new Stack<TreeNode>();
+        currLevel.push(root);
+
+        while (!currLevel.isEmpty()) {
+            List<Integer> levelRes = new ArrayList<Integer>();
+
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.pop();
+                levelRes.add(node.val);
+                if (levelCount % 2 == 0) {
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                }
+                else {
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                }
+            }
+            result.add(levelRes);
+            levelCount++;
+            Stack<TreeNode> temp = currLevel;
+            currLevel = nextLevel;
+            nextLevel = temp;
+        }
+        return result;
+    }
+}
