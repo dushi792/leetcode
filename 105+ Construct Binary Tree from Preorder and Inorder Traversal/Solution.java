45a7560c7ba4c044cdbe0e5543359c8ba1f6351b
Jun Zhao <dushi792@gmail.com>
2015-09-05 14:55:17 -0400

    105

--- 105+ Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
+++ 105+ Construct Binary Tree from Preorder and Inorder Traversal/Solution.java
@@ -26,8 +26,12 @@ public class Solution {
         //     中序 4 2 5 1 6 3 7
         // 当分解到   2 4 5
         //         4 2 5   时，
-        // 2是index, 5是index+1，如果 2 4 5 和 4 2 5 上下对齐的话，对于先序来讲preStart= index+1
-        // 但是现在并不对齐，preorder和inorder之间差 1 = preStart - inStart, 所以再上这部分偏移量
+        // 2是index, 5是index+1，如果 2 4 5 和 4 2 5 上下对齐的话，对于root.right的先序来讲
+        // preStart= index+1，但是现在并不对齐，preorder和inorder之间差 1 = preStart - inStart,
+        // 所以再上这部分偏移量
+        //
+        // 理解2： 其实preStart-inStart+index+1 就是 preEnd - (inEnd - postiton) + 1
+        // 从后往前看
         root.right = helper(preorder, inorder, preStart-inStart+index+1, index+1, inEnd);
         return root;
     }
