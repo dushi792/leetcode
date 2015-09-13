335980591db5752021affcb1f893588d144b680e
Jun Zhao <dushi792@gmail.com>
2015-09-13 14:31:19 -0400

    92

--- 92 Reverse Linked List II/Solution.java
+++ 92 Reverse Linked List II/Solution.java
@@ -28,7 +28,9 @@ public ListNode reverseBetween(ListNode head, int m, int n) {
     ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
     dummy.next = head;
     ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
-    for(int i = 0; i<m-1; i++) pre = pre.next;
+    for(int i = 1; i < m; i++) {
+      pre = pre.next;
+    }
 
     ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
     ListNode then = start.next; // a pointer to a node that will be reversed
