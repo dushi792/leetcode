76a075194868be72ecaeca9d07e76f9ebe9e7281
Jun Zhao <dushi792@gmail.com>
2015-09-14 19:56:01 -0400

    109

--- 109+ Convert Sorted List to Binary Search Tree/Solution.java
+++ 109+ Convert Sorted List to Binary Search Tree/Solution.java
@@ -1,20 +1,52 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// 优化结构
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: a tree node
+     */
+    private ListNode curr;
+
+    private int getLength(ListNode head) {
+        int length = 0;
+
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+
+        return length;
+    }
+
+    private TreeNode helper(int size) {
+        if (size <= 0) {
+            return null;
+        }
+
+        TreeNode left = helper(size / 2);
+        TreeNode root = new TreeNode(curr.val);
+        curr = curr.next;
+        TreeNode right = helper(size - 1 - size / 2);
+
+        root.left = left;
+        root.right = right;
+
+        return root;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        curr = head;
+        int size = getLength(head);
+
+        return helper(size);
+    }
+}
+
+
+// Old version
 public class Solution {
     private ListNode node;
     public TreeNode sortedListToBST(ListNode head) {
@@ -27,10 +59,10 @@ public class Solution {
             count++;
             cur = cur.next;
         }
-        
+
         return helper(0, count-1);
     }
-    
+
     private TreeNode helper(int l, int r) {
         if (l > r)
             return null;
@@ -39,10 +71,10 @@ public class Solution {
         TreeNode root = new TreeNode(node.val);
         root.left = left;
         node = node.next;
-        
+
         TreeNode right = helper(mid+1, r);
         root.right = right;
-        
+
         return root;
     }
 }
