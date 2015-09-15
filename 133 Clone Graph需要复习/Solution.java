93591810599df08e51d6921eacbf3cfb6216dbe9
Jun Zhao <dushi792@gmail.com>
2015-09-14 21:19:24 -0400

    133

--- 133 Clone Graph需要复习/Solution.java
+++ 133 Clone Graph需要复习/Solution.java
@@ -1,3 +1,43 @@
+// Iterative
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        ArrayList<UndirectedGraphNode> nodes = new ArrayList<>();
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+
+        nodes.add(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+
+        int index = 0;
+        // clone nodes
+        while (index < nodes.size()) {
+            UndirectedGraphNode head = nodes.get(index++);
+            for (int i = 0; i < head.neighbors.size(); i++) {
+                UndirectedGraphNode neighbor = head.neighbors.get(i);
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                    nodes.add(neighbor);
+                }
+            }
+        }
+
+        // clone edges
+        for (int i = 0; i < nodes.size(); i++) {
+            UndirectedGraphNode newHead = map.get(nodes.get(i));
+            for (int j = 0; j < nodes.get(i).neighbors.size(); j++) {
+                newHead.neighbors.add(map.get(nodes.get(i).neighbors.get(j)));
+            }
+        }
+
+        return map.get(node);
+    }
+}
+
+
+// Recursive
 public class Solution {
     private HashMap<Integer, UndirectedGraphNode> map
             = new HashMap<Integer, UndirectedGraphNode>();
