948929680a6253d55687f1fbfebd68a16986297e
Jun Zhao <dushi792@gmail.com>
2015-09-20 16:11:20 -0400

    51

--- 51+ N-Queens/Solution.java
+++ 51+ N-Queens/Solution.java
@@ -1,3 +1,70 @@
+// Optimized:
+class Solution {
+    private ArrayList<String> drawChessBoard(ArrayList<Integer> cols) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        for (int i = 0; i < cols.size(); i++) {
+            String row = new String();
+            for (int j = 0; j < cols.size(); j++) {
+                if (j == cols.get(i)) {
+                    row += "Q";
+                }
+                else {
+                    row += ".";
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+
+    private boolean isValid(ArrayList<Integer> cols, int col) {
+        int row = cols.size();
+        for (int i = 0; i < row; i++) {
+            if (cols.get(i) == col) {
+                return false;
+            }
+            if (i - cols.get(i) == row - col) {
+                return false;
+            }
+            if (i + cols.get(i) == row + col) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void helper(int n, ArrayList<Integer> cols, ArrayList<ArrayList<String>> res) {
+        if (cols.size() == n) {
+            res.add(drawChessBoard(cols));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!isValid(cols, i)) {
+                continue;
+            }
+            cols.add(i);
+            helper(n, cols, res);
+            cols.remove(cols.size() - 1);
+        }
+
+    }
+
+    ArrayList<ArrayList<String>> solveNQueens(int n) {
+        ArrayList<ArrayList<String>> res = new ArrayList<ArrayList<String>>();
+
+        if (n <= 0) {
+            return res;
+        }
+
+        helper(n, new ArrayList<Integer>(), res);
+
+        return res;
+    }
+};
+
+
 // Reference：http://blog.csdn.net/linhuanmars/article/details/20667175
 public class Solution {
     public List<List<String>> solveNQueens(int n) {
